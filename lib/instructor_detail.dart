68154608b78f2a3aa100f2c4d9dc9a534bc272c7
rebixpl <rebixcraft@gmail.com>
2020-06-13 09:09:12 +0200

    DONE

--- lib/instructor_detail.dart
+++ lib/instructor_detail.dart
@@ -242,7 +242,7 @@ class _InstructorDetailsState extends State<InstructorDetails> {
             child: Padding(
               padding: const EdgeInsets.only(
                 top: 10.0,
-                left: 10.0,
+                right: 10.0,
               ),
               child: Align(
                 alignment: Alignment.topRight,
